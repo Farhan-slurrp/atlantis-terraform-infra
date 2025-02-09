04edc5e82c8bc536d0c3c83a9f86bd0a2e2b6d8c
Farhan Ahmad Nurzi <farhanlmntrix@gmail.com>
2025-02-09 22:58:04 +0800

    init commit

---
+++ ec2-test/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+resource "aws_security_group" "atlantis_sg" {
+  name = "atlantis_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4141
+    to_port     = 4141
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami= "ami-0474ac020852b87a9"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.atlantis_sg.name]
+  
+  associate_public_ip_address = true  
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
